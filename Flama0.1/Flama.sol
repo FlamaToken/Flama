3d1c865011a7cf9786e78f18bb934c85d6d15a65
caduceus33 <34582277+caduceus33@users.noreply.github.com>
2020-06-23 20:05:48 +0200

    Update Flama.sol

--- Flama0.1/Flama.sol
+++ Flama0.1/Flama.sol
@@ -29,7 +29,7 @@ import "./Address.sol";
  * functions have been added to mitigate the well-known issues around setting
  * allowances. See {IERC20-approve}.
  */
-contract ERC20 is Context, IERC20 {
+contract Flama is Context, IERC20 {
     using SafeMath for uint256;
     using Address for address;
 
@@ -63,7 +63,7 @@ contract ERC20 is Context, IERC20 {
         _symbol = symbol;
         _decimals = 18;
         _owner = msg.sender;
-        _mint(_owner, 1000000);
+        _mint(_owner, 50000000);
     }
     
     function setStakingAccount(address newStakingAccount) public onlyOwner {
@@ -119,6 +119,9 @@ contract ERC20 is Context, IERC20 {
     function balanceOf(address account) public view override returns (uint256) {
         return _balances[account];
     }
+
+    
+
 
     /**
      * @dev See {IERC20-transfer}.
@@ -329,4 +332,73 @@ contract ERC20 is Context, IERC20 {
      * To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
      */
     function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual { }
+
+
+}
+
+
+
+contract StakingFlama {
+    
+    
+    using SafeMath for uint256;
+    
+    address public owner;
+    Flama public flamacontract;
+    
+    uint256 public totalinStaking;
+    mapping (address => uint256) public staked;
+    
+
+    event Staked(address indexed user, uint256 amount, uint256 total);
+    event Unstaked(address indexed user, uint256 amount, uint256 total);
+    
+    constructor (address tokenAddress) public {
+        flamacontract = Flama(tokenAddress);
+        owner = msg.sender;
+        
+    }
+
+    function stake(uint256 amount) public {
+        
+        stakeFor(msg.sender, amount);
+    }
+    
+    
+    function stakeFor(address user, uint256 amount) public {
+        
+        require(flamacontract.transferFrom(user, address(this), amount), "Stake required");
+        staked[user] = staked[user].add(amount);
+        totalinStaking = totalinStaking.add(amount);
+        emit Staked(user, amount, totalinStaking);
+        
+    }
+    
+    
+    
+    function unstake(uint256 amount) public {
+        
+        unstakeFor(msg.sender, amount);
+    }
+   
+    
+    function unstakeFor(address beneficiary, uint256 amount) public {
+        require(msg.sender == beneficiary);
+        require(staked[beneficiary] >= amount);
+        
+        
+        
+        
+        uint256 stakebalance = flamacontract.balanceOf(address(this));
+        
+        uint256 ratio = stakebalance.mul(amount);
+        uint256 unstakeAmount = ratio.div(totalinStaking);
+        
+        staked[beneficiary] = staked[beneficiary].sub(amount);
+        totalinStaking = totalinStaking.sub(amount);
+        require(flamacontract.transfer(beneficiary, unstakeAmount));
+        emit Unstaked(msg.sender, amount, totalinStaking);
+
+        
+    }
 }
